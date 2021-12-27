53c5c180e83d1c3bec814590fe92b65312e3c562
EC2 Default User <ec2-user@ip-172-31-9-110.ec2.internal>
2021-12-27 09:13:02 +0000

    [Create]使用する全てのcontroller・routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,55 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # public側のルーティング
+  
+  scope module: :public do
+    
+  #homes_controller
+  root to: 'homes#top'
+  get 'homes/about'
+
+  #items_controller
+  resources :items, only:[:index, :show]
+
+  #customers_controller
+  resource :customers, only:[:edit, :update] do
+    collection do
+      get   :my_page
+      get   :unsubscribe_confirmation
+      patch :unsubscribe
+    end
+  end
+
+  #cart_items_controller
+  resources :cart_items, only:[:index, :update, :destroy, :create] do
+    collection do
+      delete :destroy_all
+    end
+  end
+
+  #orders_controller
+  resources :orders, only:[:new, :index, :show, :create] do
+    collection do
+      post :confirm
+      get  :thanks
+    end
+ end
+ 
+ #addresses_controller
+ resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+ end
+
+ #admin側のルーティング
+
+ namespace :admin do
+   root to: 'homes#top' #homes_controller
+   resources :items, except:[:destroy] #items_controller
+   resources :genres, only:[:index, :create, :edit, :update] #genres_controller
+   resources :customers, only:[:index, :show, :edit, :update] #customers_controller
+   resources :orders, only:[:show, :update] #orders_controller
+   resources :order_details, only:[:update] #order_details_controller
+ end
+
 end
